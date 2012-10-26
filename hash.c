0113ac31d0ed0e0f38802f4ab07ee487876374d6
Robert Gruener <gruene@cooper.edu>
2012-10-26 01:15:57 -0400

    fixed bugs:

--- hash.c
+++ hash.c
@@ -4,11 +4,12 @@
 
 #include "hash.h"
 
-struct hashTable * newHashTable(int size){
+struct hashTable * hashTable_new(int size){
     if (size <= 0){
         size = getPrime(size);
     }
-    if ((struct hashTable *table = malloc(sizeof(struct hashTable))) == NULL){
+    struct hashTable *table;
+    if ((table = malloc(sizeof(struct hashTable))) == NULL){
         fprintf(stderr, "Error While Allocating Memory: %s\n", strerror(errno));
         return NULL;
     }
@@ -16,7 +17,7 @@ struct hashTable * newHashTable(int size){
     table->filled = 0;
     if ((table->data = calloc(table->capacity, sizeof(struct hashItem))) == NULL){
         fprintf(stderr, "Error While Allocating Memory: %s\n", strerror(errno));
-        return NULL'
+        return NULL;
     }
     int i;
     for (i=0; i<table->capacity; i++){
@@ -25,10 +26,10 @@ struct hashTable * newHashTable(int size){
     return table;
 }
 
-int insert(struct hashTable *table, char *key, void *pv){
+int hashTable_insert(struct hashTable *table, char *key, void *pv){
     int index = hash(table, key);
     if (table->filled >= table->capacity / 2){
-        if (rehash(table) == 0){
+        if (hashTable_rehash(table) == 0){
             return 2;
         }
     }
@@ -51,15 +52,15 @@ int insert(struct hashTable *table, char *key, void *pv){
     return 0;
 }
 
-int contains(struct hashTable *table, char *key){
-    if (findPos(table, key) == -1){
+int hashTable_contains(struct hashTable *table, char *key){
+    if (hashTable_findPos(table, key) == -1){
         return 0;
     }
     return 1;
 }
 
-void * getPointer(struct hashTable *table, char *key, int *b){
-    int index = findPos(table, key);
+void * hashTable_getPointer(struct hashTable *table, char *key, int *b){
+    int index = hashTable_findPos(table, key);
     if (index == -1){
         if (b != NULL){
             *b = 0;
@@ -72,8 +73,8 @@ void * getPointer(struct hashTable *table, char *key, int *b){
     return table->data[index].pv;
 }
 
-int setPointer(struct hashTable *table, char *key, void *pv){
-    int index = findPos(table, key);
+int hashTable_setPointer(struct hashTable *table, char *key, void *pv){
+    int index = hashTable_findPos(table, key);
     if (index == -1){
         return 1;
     }
@@ -81,8 +82,8 @@ int setPointer(struct hashTable *table, char *key, void *pv){
     return 0;
 }
 
-int remove(struct hashTable *table, char *key){
-    int index = findPos(table, key);
+int hashTable_remove(struct hashTable *table, char *key){
+    int index = hashTable_findPos(table, key);
     if (index == -1){
         return 0;
     }
@@ -102,7 +103,7 @@ int hash(struct hashTable *table, char *key){
     return hashVal;
 }
 
-int findPos(struct hashTable *table, char *key){
+int hashTable_findPos(struct hashTable *table, char *key){
     int index = hash(table, key);
     while (table->data[index].isOccupied){
         if (!strcmp(table->data[index].key, key) && !table->data[index].isDeleted){
@@ -117,9 +118,10 @@ int findPos(struct hashTable *table, char *key){
     return -1;
 }
 
-int rehash(struct hashTable *table){
+int hashTable_rehash(struct hashTable *table){
     int old_data_size = table->capacity;
-    if ((struct hashItem *old_data = calloc(table->capacity, sizeof(struct hashItem))) == NULL){
+    struct hashItem *old_data;
+    if ((old_data = calloc(table->capacity, sizeof(struct hashItem))) == NULL){
         fprintf(stderr, "Error While Allocating Memory: %s\n", strerror(errno));
         return 0;
     }
@@ -137,7 +139,7 @@ int rehash(struct hashTable *table){
     table->filled = 0;
     for (i=0; i<old_data_size; i++){
         if (old_data[i].isOccupied && !old_data[i].isDeleted){
-            insert(table, old_data[i].key, old_data[i].pv);
+            hashTable_insert(table, old_data[i].key, old_data[i].pv);
             table->filled++;
         }
     }
